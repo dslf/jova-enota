6bbd20fe27f5518816c9429b2df4dda2c45d0c50
dslf <dslf@live.ru>
2017-03-13 03:53:28 +0300

    some logic

--- Calculate.java
+++ Calculate.java
@@ -1,7 +1,13 @@
 public class Calculate {
 
   public static void main(String[] arg) {
-    System.out.println("Calculate...");
+//    System.out.println("Calculate...");
+    int st = Integer.valueOf(arg[0]);
+    int nd = Integer.valueOf(arg[1]);
+    
+    int sum = st + nd;
+    
+    System.out.println(sum);
   }
 
 }
